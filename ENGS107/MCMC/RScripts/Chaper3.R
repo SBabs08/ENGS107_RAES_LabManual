3b8d78e20e8373d8a0d73f4b362411a748ef24cb
SBabs08 <107976299+SBabs08@users.noreply.github.com>
2023-01-04 07:17:08 -0500

    Updated for ENGS107

---
+++ ENGS107/MCMC/RScripts/Chaper3.R
@@ -0,0 +1,220 @@
+# Estimate the log likelihood of the AR1 process
+logl.ar1 <- function(r, sigma1, rho1, eps1 = y.meas.err){
+  n <- length(r) # r is the residuals
+  logl <- 0
+  if(n>1) {
+    # Whiten the residuals (w)
+    w <- r[2:n] - rho1*r[1:(n-1)]
+    logl <- logl + sum(dnorm(w, sd=sqrt((sigma1)^2 + (eps1[c(-1)])^2), log=TRUE))
+  }
+  return(logl)
+}
+
+## Tutorial
+# Clear away any existing variables or figures.  
+rm(list = ls())
+graphics.off()
+# Install and read in packages.
+# install.packages("adaptMCMC")
+library(adaptMCMC)
+library(compiler)
+enableJIT(3)
+# Set the seed.
+set.seed(111)
+# Make directory for this lab
+dir.create("lab_X")
+dir.path <- paste(getwd(), "/lab_X/", sep="")
+scrim.git.path <- "https://raw.githubusercontent.com/scrim-network/"
+# Global mean sea level from Church and White (2011)
+# year, time in years from 1880 to 2013 at the half year mark
+# slr, global mean sea level in mm
+# err.obs, global mean sea level measurement error in mm
+url <- paste(scrim.git.path, "BRICK/master/data/GMSL_ChurchWhite2011_yr_2015.txt", sep="")
+download.file(url, paste(dir.path, "GMSL_ChurchWhite2011_yr_2015.txt", sep=""))
+church.data <- read.table("lab_X/GMSL_ChurchWhite2011_yr_2015.txt")
+year <- church.data[ ,1] - 0.5 
+slr <- church.data[ ,2]
+err.obs <- church.data[ ,3]
+# Calculate sea level -/+ observation errors.
+err_pos <- slr + err.obs
+err_neg <- slr - err.obs
+# Read in the information from the Smith (2008) temperature file.
+# project.years, time in years from 1880 to 2100.
+# hist.temp, historical global mean temperatures from 1880 to 2013 in C
+# rcp85, merged historical + rcp 8.5 temperatures from 1880 to 2100 in C 
+url <- paste(scrim.git.path, 
+             "Ruckertetal_SLR2016/master/RFILES/Data/NOAA_IPCC_RCPtempsscenarios.csv", 
+             sep="")
+download.file(url, paste(dir.path, "NOAA_IPCC_RCPtempsscenarios.csv", sep=""))
+temp.data <- read.csv("lab_X/NOAA_IPCC_RCPtempsscenarios.csv")
+project.years <- temp.data[1:match(2100, temp.data[,1]), 1]
+hist.temp <- temp.data[1:match(2013, temp.data[,1]), 2]
+rcp85 <- temp.data[1:match(2100, temp.data[,1]), 4]
+# The sea level and temperature data have a yearly timestep.
+tstep = 1
+# Load the sea-level model (Rahmstorf, 2007)
+url <- paste(scrim.git.path, "BRICK/BRICKms/R/gmsl_r07.R", sep="")
+download.file(url, paste(dir.path, "gmsl_r07.R", sep=""))
+source("lab_X/gmsl_r07.R")
+
+### Characterizing the error structure
+par(mfrow = c(1,1))
+plot(year, err.obs, type = "l", ylab = "Observation errors [mm]", xlab = "Time")
+points(year, err.obs, pch = 20)
+
+# Generate fit to sea-level observations using General-purpose Optimization.
+# by calculating the root mean squared error.
+fn <- function(pars, tstep, Temp, obs){
+  a   <- pars[1]
+  Teq <- pars[2]
+  SL0 <- pars[3]
+  np      <- length(Temp)
+  gmsl    <- rep(NA, np)
+  gmsl[1] <- SL0
+  
+  # Use Forward Euler to estimate sea level over time.
+  for (i in 2:np) {
+    gmsl[i] <- gmsl[i-1]  +  tstep * a * ( Temp[i-1]-Teq )
+  }
+  resid <- obs - gmsl
+  # Return the root mean square error
+  rmse <- sqrt(mean(resid^2))
+  return(rmse) 
+}
+# Plug in random values for the parameters.
+parameter_guess <- c(3.4, -0.5, slr[1])
+# Optimize the model to find initial starting values for parameters.
+result <- optim(parameter_guess, fn, gr=NULL, tstep, Temp = hist.temp, obs = slr)
+start_alpha <- result$par[1]
+start_Teq <- result$par[2]
+start_SL0 <- result$par[3]
+# Make a plot of observations and fit.
+slr.est <- gmsl_r07(a = start_alpha, Teq = start_Teq, SL0 = start_SL0, tstep, 
+                    Tg = hist.temp)
+plot(year, slr, pch = 20, ylab = "Sea-level Anomaly [mm]", xlab = "Year")
+lines(year, slr.est, col = "blue", lwd = 2)
+# Calculate residuals and initial sigma value.
+res <- slr - slr.est
+start_sigma <- sd(res)
+
+# Apply the auto-correlation function to determine correlation coefficients.
+rho <- rep(NA,3)
+ac <- acf(res, lag.max = 5, plot = TRUE, main = "", xlab = "Time lag", 
+          ylab = "Autocorrelation function")
+rho[1] <- ac$acf[1]
+rho[2] <- ac$acf[2]
+rho[3] <- ac$acf[3]
+rho[4] <- ac$acf[4]
+rho[5] <- ac$acf[5]
+start_rho <- rho[2]
+
+### Setting up the prior information
+# Set up MCMC calibration.
+# Set up priors.
+bound.lower <- c( 0, -3, err_neg[1], 0, -0.99)
+bound.upper <- c(20,  2, err_pos[1], 10,  0.99)
+# Name the parameters and specify the number of physical model parameters.
+# Sigma and rho are statistical parameters and will not be counted in the number.
+parnames <- c("alpha", "Teq", "SL0", "sigma", "rho")
+model.p <- 3
+# Set the measurement errors.
+y.meas.err <- err.obs
+# Load the likelihood model accounting for the error structure.
+source("Rahm_obs_likelihood_AR.R")
+# Setup initial parameters.
+p <- c(start_alpha, start_Teq, start_SL0, start_sigma, start_rho) 
+# Set p0 to a vector of values to be used in optimizing the log-posterior function.
+# We optimize the negative log-posterior function because “optim” only knows how to 
+# minimize a function, while we would like the log-posterior to be maximized.
+p0 <- c(3.4, -0.5, slr[1], 6, 0.5)
+p0 <- optim(p0, function(p) -log.post(p))$par
+print(round(p0, 4))
+
+### Running MCMC
+# Set optimal acceptance rate as # parameters->infinity 
+# (Gelman et al, 1996; Roberts et al, 1997)
+# Set number of iterations and rate of adaptation 
+# (gamma.mcmc, between 0.5 and 1, lower is faster adaptation)
+accept.mcmc = 0.234
+gamma.mcmc = 0.5
+NI <- 4e5
+step <- c(0.2, 0.02, 1, 0.1, 0.01)
+# Run MCMC calibration. Note: Runs for several minutes.
+mcmc.out <- MCMC(log.post, NI, p0, scale = step, adapt = TRUE, acc.rate = accept.mcmc, 
+                 gamma = gamma.mcmc, list = TRUE, n.start = round(0.01*NI))
+mcmc.chains <- mcmc.out$samples
+
+### Determine the burn-in period
+## Gelman and Rubin's convergence diagnostic:
+set.seed(1708)
+p0 <- c(1.9, -0.9, -145, 4, 0.7) # Arbitrary choice.
+mcmc.out2 <- MCMC(log.post, NI, p0, scale = step, adapt = TRUE, acc.rate = accept.mcmc, 
+                  gamma = gamma.mcmc, list = TRUE, n.start = round(0.01*NI))
+mcmc.chains2 <- mcmc.out2$samples
+set.seed(1234)
+p0 <- c(2.9, 0, -160, 5, 0.8) # Arbitrary choice.
+mcmc.out3 <- MCMC(log.post, NI, p0, scale = step, adapt = TRUE, acc.rate = accept.mcmc, 
+                  gamma = gamma.mcmc, list = TRUE, n.start = round(0.01*NI))
+mcmc.chains3 <- mcmc.out3$samples
+# Turn the chains into an mcmc object.
+# This is a requirement for the gelman.diag and gelman.plot commmand.
+mcmc1 <- as.mcmc(mcmc.chains) 
+mcmc2 <- as.mcmc(mcmc.chains2) 
+mcmc3 <- as.mcmc(mcmc.chains3)
+# Test for convergence.
+set.seed(111) # revert back to original seed
+mcmc_chain_list <- mcmc.list(list(mcmc1, mcmc2, mcmc3))
+gelman.diag(mcmc_chain_list)
+gelman.plot(mcmc_chain_list)
+# Create a vector to test the statistic at several spots throughout the chain.
+# Test from 5000 to 4e5 in increments of 5000.
+niter.test <- seq(from = 5000, to = NI, by = 5000)
+gr.test <- rep(NA, length(niter.test))
+# Once the statistic is close to 1 (adopting the standard of < 1.1), the between-chain 
+# variability is indistinguishable from the within-chain variability, and hence converged.
+for (i in 1:length(niter.test)){
+  mcmc1 = as.mcmc(mcmc.chains[1:niter.test[i],])
+  mcmc2 = as.mcmc(mcmc.chains2[1:niter.test[i],])
+  mcmc3 = as.mcmc(mcmc.chains3[1:niter.test[i],])
+  mcmc_chain_list = mcmc.list(list(mcmc1, mcmc2, mcmc3))
+  gr.test[i] = gelman.diag(mcmc_chain_list)[2]
+}
+# Plot Gelman and Rubin statistics as a function of iterations. The iterations prior  
+# to the point in which convergence happens is what we set as the burn-in.
+plot(niter.test, gr.test, pch=20)
+abline(h = 1.1, col = "red")
+gr.test < 1.1; first.stat = max(which((gr.test < 1.1) == FALSE))+1; print(first.stat)
+# Set the burn-in to the 1st statistic that remains under 1.1 and remove it.
+burnin <- seq(1, niter.test[first.stat], 1)
+slr.burnin.chain <- mcmc.chains[-burnin, ]
+
+### Hindcasting and projecting sea-level rise
+# Extract parameter vectors from the chain to enhance code readability.
+alpha.chain <- slr.burnin.chain[ ,1]
+Teq.chain <-   slr.burnin.chain[ ,2]
+SL_0.chain <-  slr.burnin.chain[ ,3]
+sigma.chain <- slr.burnin.chain[ ,4]
+rho.chain <-   slr.burnin.chain[ ,5]
+# Set up empty matrices for sea-level output.
+# Loop over the sea level model to generate a distribution of sea level simulations.
+NI_length <- length(slr.burnin.chain[ ,1])
+SLR.model.sim <- mat.or.vec(NI_length, length(project.years))
+for(n in 1:NI_length) {
+  SLR.model.sim[n, ] = gmsl_r07(a = alpha.chain[n], Teq = Teq.chain[n], 
+                                SL0 = SL_0.chain[n], tstep, Tg = rcp85)
+}
+# Estimate the residuals with the AR(1) coefficient and sigma.
+SLR.residuals <- mat.or.vec(NI_length, length(project.years)) #(nr,nc)
+for(n in 1:NI_length) {
+  for(i in 2:length(project.years)) {
+    SLR.residuals[n,i] <- rho.chain[n]*SLR.residuals[n,i-1] + 
+      rnorm(1, mean = 0, sd = sigma.chain[n])
+  }
+}
+# Estimate the hindcasts and projections: add the residuals onto the model simulations.
+SLR.projections <- mat.or.vec(NI_length, length(project.years)) #(nr,nc)
+for(i in 1:NI_length) {
+  SLR.projections[i,] <- SLR.model.sim[i,] + SLR.residuals[i,]
+}
+
+
---
+++ ENGS107/MCMC/RScripts/Chapter1.R
@@ -0,0 +1,186 @@
+# Set up the known probabilities based on prior knowledge.
+# Define the transition matrix.
+# Rows represent the current state.
+# Columns are the next state.
+#       FOGGY   SUNNY  RAINY
+FOGGY <- c( 0.5, 0.25,  0.25)  
+SUNNY <- c( 0.5,    0,   0.5)
+RAINY <- c(0.25, 0.25,   0.5)
+# Define the parameter space.
+parameter <- c("Foggy", "Sunny", "Rainy")
+P <- matrix(c(FOGGY, SUNNY, RAINY), nrow=3, ncol=3, byrow = TRUE, 
+            dimnames = list(parameter, parameter))
+print(P)
+
+# PREDICTING THE WEATHER WITH A MARKOV CHAIN EXAMPLE
+#================================================
+# Set the prediction length and the initial parameter value.
+Prediction_Days <- 25
+CurrentStateProb <- mat.or.vec(Prediction_Days, length(P[1,]))
+CurrentStateProb[1,] <- c(1, 0, 0) # Today is foggy
+# Run the Markov chain.
+for(i in 2:Prediction_Days){
+  # Current parameter value times probability
+  Prob <- CurrentStateProb[i-1, ] * P
+  CurrentStateProb[i, ] <- c(sum(Prob[,1]), sum(Prob[,2]), sum(Prob[,3]))
+}
+colnames(CurrentStateProb) <- parameter
+print(CurrentStateProb)
+# Print weather predictions.
+print(paste("p(", parameter, ") in 1 day = ", round(CurrentStateProb[2, ], 2)))
+print(paste("p(", parameter, ") in 5 days = ", round(CurrentStateProb[6, ], 2)))
+print(paste("p(", parameter, ") in 24 days = ", round(CurrentStateProb[25, ], 2)))
+
+
+# SAMPLING THE PROBABILITY DISTRIBUTION; A MARKOV CHAIN MONTE CARLO EXAMPLE
+#================================================
+# Set the initial parameter value probability.
+CurrentState <- sample(parameter, 1, prob = c(0.5, 0.25, 0.25)) # Today is foggy
+# Start sampling (iteratively) from the distribution.
+weather <- c()
+for (i in 1:1e4){
+  NextState <- sample(parameter, 1, prob = P[CurrentState,])
+  weather[i] <- NextState
+  CurrentState <- NextState
+}
+# Throw away the first 1% of the data (Burn-in)
+burnin <- seq(from = 1, to = 1e4*0.1, by = 1)
+weatherDraws <- weather[-burnin]
+# DISPLAY 
+#================================================
+par(mfrow = c(1,2))
+# Display the results from the Markov Chain
+plot(1:Prediction_Days, CurrentStateProb[ ,1], xlab = "Days", ylab = "P(Weather)", 
+     ylim = c(0,1), type = "l", lwd = 2, col = "gray")
+lines(1:Prediction_Days, CurrentStateProb[ ,2], lwd = 2, col = "gold")
+lines(1:Prediction_Days, CurrentStateProb[ ,3], lwd = 2, col = "blue")
+legend("right", c("Foggy", "Sunny", "Rainy"), lwd = 2, bty = "n", col = c("gray", "gold", 
+                                                                          "blue"))
+# Display the results from MCMC
+barplot(prop.table(table(weatherDraws)), ylim = c(0,1), 
+        sub = "Equilibrium distribution\nof the weather", col = c("gray", "blue", "gold"))
+abline(h = 0.4, lty = 2); abline(h = 0.2, lty = 2)
+
+###Tutorial
+# Clear away any existing variables or figures and set the seed for random sampling.
+rm(list = ls())
+graphics.off()
+set.seed(1234)
+# Define the true model parameters and set up the time scale.
+alpha.true <- 2 # Arbitrary choices.
+beta.true <- -5
+time <- 1:10
+# Generate some observations with noise (measurement error) and the model.
+y.true = alpha.true*time + beta.true
+sigma = 1
+meas_err <- rnorm(length(time), mean = 0, sd = sigma)
+y = y.true + meas_err
+# Plot the true model and observations.
+par(mfrow = c(1,1))
+plot(time, y.true, type="l", main="True observations and model",
+     xlab = "Time", ylab = "Observations")
+points(time, y, pch = 20)
+
+####Define the log posterior
+plot(0:4, 0:4, type="n", xlab="x", ylab="f(x)", xaxt = "n", yaxt = "n")
+segments(1, -1, 1, 2.5)
+segments(3, -1, 3, 2.5)
+segments(1, 2.5, 3, 2.5, lty=2)
+axis(1, c(0, 1,3), labels=c(0,"a", "b"))
+axis(2, 2.5, labels=expression(frac(1,b-a)), las=1)
+
+# Define the unnormalized log posterior.
+logp = function(theta){
+  N = length(time)
+  
+  # Calulate model simulations.
+  alpha = theta[1]; beta = theta[2]
+  model = alpha*time + beta
+  
+  # Estimate the residuals (i.e., the deviation of the observations from the 
+  # model simulation).
+  resid = 	y - model
+  
+  # Get the log of the likelihood function.
+  log.likelihood = -N/2*log(2*pi) - N*log(sigma) - 1/2*sum(resid^2)/sigma^2
+  
+  # Use an improper uniform "relatively uninformative" prior.
+  log.prior = 0 # log(1)
+  
+  # Bayesian updating: update the probability estimates based on the observations.
+  log.posterior = log.likelihood + log.prior
+  
+  # Return the unnormalized log posterior value. 
+  return(log.posterior)
+}
+
+#####Constructing a Markov Chain
+# Set the number of MCMC iterations.
+NI = 30000
+# Start with some initial state of parameter estimates, theta^initial
+alpha.init = runif(1, -50, 50) # Arbitrary choices.
+beta.init = runif(1, -50, 50)  
+theta = c(alpha.init, beta.init)
+```
+
+Using the initial parameter values $\theta^{initial}$, you then evaluate the unnormalized posterior of that value, $p(\theta^{initial} \mid y)$, using the likelihood function ($L(y \mid \theta)$) and the prior distribution.
+```{r eval=FALSE}
+# Evaluate the unnormalized posterior of the parameter values
+# P(theta^initial | y)
+lp = logp(theta)
+
+# Setup some variables and arrays to keep track of:
+theta.best = theta         # the best parameter estimates
+lp.max = lp                # the maximum of the log posterior
+theta.new = rep(NA,2)      # proposed new parameters (theta^new)
+accepts = 0                # how many times the proposed new parameters are accepted
+mcmc.chains = array(dim=c(NI,2)) # and a chain of accepted parameters
+# Set the step size for the MCMC.
+step = c(0.1, 1)
+# Metropolis-Hastings algorithm MCMC; the proposal distribution proposes the next 
+# point to which the random walk might move. For this algorithm, this proposal  
+# distribution is symmetric, that is P(x to x`) = P(x` to x).
+for(i in 1:NI) {
+  # Propose a new state (theta^new) based on the current parameter values 
+  # theta and the transition probability / step size
+  theta.new = rnorm(2, theta, sd = step)
+  
+  # Evaluate the new unnormalized posterior of the parameter values
+  # and compare the proposed value to the current state
+  lp.new = logp(theta.new)
+  lq = lp.new - lp
+  # Metropolis test; compute the acceptance ratio
+  # Draw some uniformly distributed random number 'lr' from [0,1]; 
+  lr = log(runif(1))
+  
+  # If lr < the new proposed value, then accept the parameters setting the 
+  # proposed new theta (theta^new) to the current state (theta).
+  if(lr < lq) {
+    # Update the current theta and log posterior to the new state.
+    theta = theta.new
+    lp = lp.new
+    
+    # If this proposed new parameter value is “better” (higher posterior probability)
+    # than the current state, then accept with a probability of 1. Hence, increase
+    # the number of acceptions by 1.
+    accepts = accepts + 1
+    
+    # Check if the current state is the best, thus far and save if so.
+    if(lp > lp.max) {
+      theta.best = theta
+      lp.max = lp	
+    }
+  }
+  # Append the parameter estimate to the chain. This will generate a series of parameter
+  # values (theta_0, theta_1, ...). 
+  mcmc.chains[i,] = theta
+}
+
+
+#####Checking the acceptance rate
+# Calculate the parameter acceptance rate; it should be higher than 23.4%.
+accept.rate <- (accepts/NI) * 100
+print(accept.rate)
+
+
+
